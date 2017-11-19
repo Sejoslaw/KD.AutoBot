c9a22f60728ebf0144036bd41b07e5f0fc4f419e
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-11-19 11:05:19 +0100

    Added Windows implementation of IConnectionHandler module.

---
+++ KD.AutoBot.Connection.Windows/WindowsConnectionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace KD.AutoBot.Connection.Windows
+{
+    /// <summary>
+    /// Windows implementation of <see cref="IConnectionHandler"/>.
+    /// </summary>
+    public class WindowsConnectionHandler : AbstractConnectionHandler
+    {
+        public WindowsConnectionHandler(IAutoBot bot, IPlatformConnectionTools platformConnectionTools) :
+            base(bot, platformConnectionTools, new HashSet<IConnectedProcess>())
+        {
+        }
+
+        public WindowsConnectionHandler(IAutoBot bot, IPlatformConnectionTools platformConnectionTools, ICollection<IConnectedProcess> connectedProcesses) :
+            base(bot, platformConnectionTools, connectedProcesses)
+        {
+        }
+
+        public override bool AttachToProcess(Process process)
+        {
+            bool value = this.PlatformConnectionTools.AttachToProcess(process);
+            return value;
+        }
+    }
+}
---
+++ KD.AutoBot.Connection.Windows/WindowsPlatformConnectionTools.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace KD.AutoBot.Connection.Windows
+{
+    /// <summary>
+    /// Windows implementation of <see cref="IPlatformConnectionTools"/>.
+    /// </summary>
+    public class WindowsPlatformConnectionTools : AbstractPlatformConnectionTools
+    {
+        public WindowsPlatformConnectionTools(IConnectionHandler connectionHandler) :
+            base(connectionHandler, "Windows")
+        {
+        }
+
+        public override bool AttachToProcess(Process process)
+        {
+            if (process == null)
+            {
+                return false;
+            }
+
+            IConnectedProcess connectedProcess = new BasicConnectedProcess(this.ConnectionHandler, process);
+            this.ConnectionHandler.ConnectedProcesses.Add(connectedProcess);
+            return true;
+        }
+    }
+}
