0cdd6a1ec7c9c0adf0e58a851af1b4611b23bdb6
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-12-01 19:58:14 +0100

    Added LearningModule based on NeuralNetwork.

---
+++ KD.AutoBot.AI/INeuralNetworkLearningModule.cs
@@ -0,0 +1,13 @@
+﻿namespace KD.AutoBot.AI
+{
+    /// <summary>
+    /// <see cref="ILearningModule"/> which uses <see cref="INeuralNetwork{TNeuronDataType}"/> as a base of learning.
+    /// </summary>
+    public interface INeuralNetworkLearningModule<TNeuronDataType> : ILearningModule
+    {
+        /// <summary>
+        /// Neural Network connected with this module.
+        /// </summary>
+        INeuralNetwork<TNeuronDataType> NeuralNetwork { get; }
+    }
+}
