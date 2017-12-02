ad6081b94563a1e31446d4c0fe7b478c410a5e85
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-12-02 20:56:44 +0100

    Changed protection level in Windows native methods and structures.

--- KD.AutoBot.Input.Windows/Native/NativeMethods.cs
+++ KD.AutoBot.Input.Windows/Native/NativeMethods.cs
@@ -6,50 +6,50 @@ namespace KD.AutoBot.Input.Windows.Native
     /// <summary>
     /// Holds native Win32 API calls.
     /// </summary>
-    public static class NativeMethods
+    internal static class NativeMethods
     {
         /// <summary>
         /// Simulates mouse buttons and wheel event to Screen.
         /// </summary>
         [DllImport("user32.dll")]
-        public static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+        internal static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
 
         /// <summary>
         /// Retrieves the cursor's position, in screen coordinates.
         /// </summary>
         [DllImport("user32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool GetCursorPos(out POINT lpPoint);
+        internal static extern bool GetCursorPos(out POINT lpPoint);
 
         /// <summary>
         /// The ClientToScreen API converts the client-area coordinates of a specified point to screen coordinates.
         /// </summary>
         [DllImport("user32.dll")]
-        public static extern bool ClientToScreen(IntPtr hWnd, ref POINT lpPoint);
+        internal static extern bool ClientToScreen(IntPtr hWnd, ref POINT lpPoint);
 
         /// <summary>
         /// Sets Cursor position.
         /// </summary>
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool SetCursorPos(int x, int y);
+        internal static extern bool SetCursorPos(int x, int y);
 
         /// <summary>
         /// Synthesizes keystrokes, mouse motions, and button clicks.
         /// </summary>
         [DllImport("user32.dll")]
-        public static extern uint SendInput(uint inputsLength, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] inputs, int inputStructureSize);
+        internal static extern uint SendInput(uint inputsLength, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] inputs, int inputStructureSize);
 
         /// <summary>
         /// Sets the focus to given Window Handler.
         /// </summary>
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr SetFocus(IntPtr hWnd);
+        internal static extern IntPtr SetFocus(IntPtr hWnd);
 
         /// <summary>
         /// The SetActiveWindow API
         /// </summary>
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr SetActiveWindow(IntPtr hWnd);
+        internal static extern IntPtr SetActiveWindow(IntPtr hWnd);
     }
 }
