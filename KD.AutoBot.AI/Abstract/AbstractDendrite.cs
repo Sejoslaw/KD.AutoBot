48291d26e8a66b8b45369c00dc63f3397b5a8e60
Krzysztof Dobrzyński <k.dobrzynski9494@gmail.com>
2017-12-03 11:04:30 +0100

    Added abstract implementation of KD.AutoBot.AI

--- KD.AutoBot.AI/Abstract/AbstractDendrite.cs
+++ KD.AutoBot.AI/Abstract/AbstractDendrite.cs
@@ -7,11 +7,5 @@ namespace KD.AutoBot.AI.Abstract
     {
         public INeuronSignal<TNeuronDataType> Input { get; protected set; }
         public INeuronWeight<TNeuronDataType> DendriteWeight { get; protected set; }
-
-        public AbstractDendrite(INeuronSignal<TNeuronDataType> input, INeuronWeight<TNeuronDataType> dendriteWeight)
-        {
-            this.Input = input;
-            this.DendriteWeight = dendriteWeight;
-        }
     }
 }
