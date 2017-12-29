6a5a44104a2d1f98a7b77fc273b31ea6d4cc884e
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2017-12-29 13:07:39 +0100

    Added skeleton of TicTacToe game.

---
+++ Game_TicTacToe/MainForm.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Game_TicTacToe
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GB_Buttons = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.GB_CurrentPlayerTurn = new System.Windows.Forms.GroupBox();
+            this.TB_CurrentPlayerTurn = new System.Windows.Forms.TextBox();
+            this.GB_Players = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_NewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.L_Players_Player1 = new System.Windows.Forms.Label();
+            this.L_PLayers_Player2 = new System.Windows.Forms.Label();
+            this.GB_Score = new System.Windows.Forms.GroupBox();
+            this.L_Score_Player2 = new System.Windows.Forms.Label();
+            this.L_Score_Player1 = new System.Windows.Forms.Label();
+            this.TB_Score_Player1 = new System.Windows.Forms.TextBox();
+            this.TB_Score_Player2 = new System.Windows.Forms.TextBox();
+            this.GB_Buttons.SuspendLayout();
+            this.GB_CurrentPlayerTurn.SuspendLayout();
+            this.GB_Players.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.GB_Score.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GB_Buttons
+            // 
+            this.GB_Buttons.Controls.Add(this.button8);
+            this.GB_Buttons.Controls.Add(this.button9);
+            this.GB_Buttons.Controls.Add(this.button10);
+            this.GB_Buttons.Controls.Add(this.button5);
+            this.GB_Buttons.Controls.Add(this.button6);
+            this.GB_Buttons.Controls.Add(this.button7);
+            this.GB_Buttons.Controls.Add(this.button3);
+            this.GB_Buttons.Controls.Add(this.button2);
+            this.GB_Buttons.Controls.Add(this.button1);
+            this.GB_Buttons.Location = new System.Drawing.Point(12, 27);
+            this.GB_Buttons.Name = "GB_Buttons";
+            this.GB_Buttons.Size = new System.Drawing.Size(178, 194);
+            this.GB_Buttons.TabIndex = 0;
+            this.GB_Buttons.TabStop = false;
+            this.GB_Buttons.Text = "Buttons";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(118, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(118, 75);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(62, 75);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 75);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 3;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(118, 131);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(62, 131);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 7;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(6, 131);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 50);
+            this.button10.TabIndex = 6;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // GB_CurrentPlayerTurn
+            // 
+            this.GB_CurrentPlayerTurn.Controls.Add(this.TB_CurrentPlayerTurn);
+            this.GB_CurrentPlayerTurn.Location = new System.Drawing.Point(197, 27);
+            this.GB_CurrentPlayerTurn.Name = "GB_CurrentPlayerTurn";
+            this.GB_CurrentPlayerTurn.Size = new System.Drawing.Size(132, 49);
+            this.GB_CurrentPlayerTurn.TabIndex = 1;
+            this.GB_CurrentPlayerTurn.TabStop = false;
+            this.GB_CurrentPlayerTurn.Text = "Current Player Turn";
+            // 
+            // TB_CurrentPlayerTurn
+            // 
+            this.TB_CurrentPlayerTurn.Location = new System.Drawing.Point(6, 19);
+            this.TB_CurrentPlayerTurn.Name = "TB_CurrentPlayerTurn";
+            this.TB_CurrentPlayerTurn.ReadOnly = true;
+            this.TB_CurrentPlayerTurn.Size = new System.Drawing.Size(116, 20);
+            this.TB_CurrentPlayerTurn.TabIndex = 0;
+            // 
+            // GB_Players
+            // 
+            this.GB_Players.Controls.Add(this.L_PLayers_Player2);
+            this.GB_Players.Controls.Add(this.L_Players_Player1);
+            this.GB_Players.Location = new System.Drawing.Point(197, 83);
+            this.GB_Players.Name = "GB_Players";
+            this.GB_Players.Size = new System.Drawing.Size(132, 65);
+            this.GB_Players.TabIndex = 2;
+            this.GB_Players.TabStop = false;
+            this.GB_Players.Text = "Players";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(343, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMI_NewGame,
+            this.TSMI_Exit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // TSMI_NewGame
+            // 
+            this.TSMI_NewGame.Name = "TSMI_NewGame";
+            this.TSMI_NewGame.Size = new System.Drawing.Size(152, 22);
+            this.TSMI_NewGame.Text = "New Game";
+            // 
+            // TSMI_Exit
+            // 
+            this.TSMI_Exit.Name = "TSMI_Exit";
+            this.TSMI_Exit.Size = new System.Drawing.Size(152, 22);
+            this.TSMI_Exit.Text = "Exit";
+            // 
+            // L_Players_Player1
+            // 
+            this.L_Players_Player1.AutoSize = true;
+            this.L_Players_Player1.Location = new System.Drawing.Point(7, 20);
+            this.L_Players_Player1.Name = "L_Players_Player1";
+            this.L_Players_Player1.Size = new System.Drawing.Size(58, 13);
+            this.L_Players_Player1.TabIndex = 0;
+            this.L_Players_Player1.Text = "Player 1: X";
+            // 
+            // L_PLayers_Player2
+            // 
+            this.L_PLayers_Player2.AutoSize = true;
+            this.L_PLayers_Player2.Location = new System.Drawing.Point(7, 38);
+            this.L_PLayers_Player2.Name = "L_PLayers_Player2";
+            this.L_PLayers_Player2.Size = new System.Drawing.Size(59, 13);
+            this.L_PLayers_Player2.TabIndex = 1;
+            this.L_PLayers_Player2.Text = "Player 2: O";
+            // 
+            // GB_Score
+            // 
+            this.GB_Score.Controls.Add(this.TB_Score_Player2);
+            this.GB_Score.Controls.Add(this.TB_Score_Player1);
+            this.GB_Score.Controls.Add(this.L_Score_Player2);
+            this.GB_Score.Controls.Add(this.L_Score_Player1);
+            this.GB_Score.Location = new System.Drawing.Point(197, 154);
+            this.GB_Score.Name = "GB_Score";
+            this.GB_Score.Size = new System.Drawing.Size(132, 67);
+            this.GB_Score.TabIndex = 4;
+            this.GB_Score.TabStop = false;
+            this.GB_Score.Text = "Score";
+            // 
+            // L_Score_Player2
+            // 
+            this.L_Score_Player2.AutoSize = true;
+            this.L_Score_Player2.Location = new System.Drawing.Point(8, 42);
+            this.L_Score_Player2.Name = "L_Score_Player2";
+            this.L_Score_Player2.Size = new System.Drawing.Size(51, 13);
+            this.L_Score_Player2.TabIndex = 3;
+            this.L_Score_Player2.Text = "Player 2: ";
+            // 
+            // L_Score_Player1
+            // 
+            this.L_Score_Player1.AutoSize = true;
+            this.L_Score_Player1.Location = new System.Drawing.Point(8, 16);
+            this.L_Score_Player1.Name = "L_Score_Player1";
+            this.L_Score_Player1.Size = new System.Drawing.Size(51, 13);
+            this.L_Score_Player1.TabIndex = 2;
+            this.L_Score_Player1.Text = "Player 1: ";
+            // 
+            // TB_Score_Player1
+            // 
+            this.TB_Score_Player1.Location = new System.Drawing.Point(66, 13);
+            this.TB_Score_Player1.Name = "TB_Score_Player1";
+            this.TB_Score_Player1.ReadOnly = true;
+            this.TB_Score_Player1.Size = new System.Drawing.Size(56, 20);
+            this.TB_Score_Player1.TabIndex = 4;
+            // 
+            // TB_Score_Player2
+            // 
+            this.TB_Score_Player2.Location = new System.Drawing.Point(66, 39);
+            this.TB_Score_Player2.Name = "TB_Score_Player2";
+            this.TB_Score_Player2.ReadOnly = true;
+            this.TB_Score_Player2.Size = new System.Drawing.Size(56, 20);
+            this.TB_Score_Player2.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 233);
+            this.Controls.Add(this.GB_Score);
+            this.Controls.Add(this.GB_Players);
+            this.Controls.Add(this.GB_CurrentPlayerTurn);
+            this.Controls.Add(this.GB_Buttons);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "TicTacToe";
+            this.GB_Buttons.ResumeLayout(false);
+            this.GB_CurrentPlayerTurn.ResumeLayout(false);
+            this.GB_CurrentPlayerTurn.PerformLayout();
+            this.GB_Players.ResumeLayout(false);
+            this.GB_Players.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.GB_Score.ResumeLayout(false);
+            this.GB_Score.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GB_Buttons;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox GB_CurrentPlayerTurn;
+        private System.Windows.Forms.TextBox TB_CurrentPlayerTurn;
+        private System.Windows.Forms.GroupBox GB_Players;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_NewGame;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_Exit;
+        private System.Windows.Forms.Label L_PLayers_Player2;
+        private System.Windows.Forms.Label L_Players_Player1;
+        private System.Windows.Forms.GroupBox GB_Score;
+        private System.Windows.Forms.Label L_Score_Player2;
+        private System.Windows.Forms.Label L_Score_Player1;
+        private System.Windows.Forms.TextBox TB_Score_Player1;
+        private System.Windows.Forms.TextBox TB_Score_Player2;
+    }
+}
+
---
+++ Game_TicTacToe/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Game_TicTacToe
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
