e2f1048f5a681d82ff384bf0fcdf1f0db8e22b95
Krzysztof Dobrzyński <k.dobrzynski9494@gmail.com>
2018-01-13 10:54:07 +0100

    Generalized how main methods should work

--- Game/TicTacToe/KD.AutoBot.Game.TicTacToe.GeneticSharp/States/MakingMoveState.cs
+++ Game/TicTacToe/KD.AutoBot.Game.TicTacToe.GeneticSharp/States/MakingMoveState.cs
@@ -21,6 +21,10 @@ namespace KD.AutoBot.Game.TicTacToe.GeneticSharp.States
         public override void PrepareNextAction(ILearningModule learningModule)
         {
             // TODO: Add logic
+            // Check if Bot is connected to game.
+            // Build board matrix.
+            // Run Genetic Alghoritm on Board.
+            // When GA decide which button to press, press it.
 
             // At the end set waiting module as current.
             learningModule.CurrentState = learningModule.States.ElementAt(0);
--- Game/TicTacToe/KD.AutoBot.Game.TicTacToe/TicTacToeAutoBot.cs
+++ Game/TicTacToe/KD.AutoBot.Game.TicTacToe/TicTacToeAutoBot.cs
@@ -1,7 +1,9 @@
 ﻿using KD.AutoBot.Connection.Windows;
+using KD.AutoBot.Connection.Windows.Native;
 using KD.AutoBot.Game.TicTacToe.GeneticSharp;
 using KD.AutoBot.Input.Windows;
 using System;
+using System.Diagnostics;
 
 namespace KD.AutoBot.Game.TicTacToe
 {
@@ -11,12 +13,46 @@ namespace KD.AutoBot.Game.TicTacToe
     [Serializable]
     internal class TicTacToeAutoBot : AbstractAutoBot
     {
+        /// <summary>
+        /// Handler for TicTacToe window.
+        /// </summary>
+        private IntPtr WindowPtr
+        {
+            get
+            {
+                return (IntPtr)this["windowPtr"];
+            }
+            set
+            {
+                this["windowPtr"] = value;
+            }
+        }
+
+        /// <summary>
+        /// TicTacToe Char used by this AutoBot.
+        /// It should be either X or O.
+        /// </summary>
+        public string TttChar
+        {
+            get
+            {
+                return this["tttChar"].ToString();
+            }
+            set
+            {
+                this["tttChar"] = value;
+            }
+        }
+
         public TicTacToeAutoBot()
         {
             this.ConnectionHandler = new WindowsConnectionHandler(this);
             this.InputHandler = new WindowsInputHandler(this, null, new WindowsMouseHandler(this.InputHandler), null);
             this.LearningModule = new TicTacToeLearningModule(this);
             this.Storage = null; // Currently nothing is saved anywhere.
+
+            this.ConnectToGame();
+            this.FindCurrentPlayerChar();
         }
 
         public override void PauseBot()
@@ -46,6 +82,20 @@ namespace KD.AutoBot.Game.TicTacToe
                     this.LearningModule.PerformNextAction();
                 }
             }
+        }
+
+        private void ConnectToGame()
+        {
+            this.WindowPtr = NativeMethodsHelper.GetWindowByTitle("TicTacToe");
+            Process windowProcess = Process.GetProcessById(this.WindowPtr.ToInt32());
+            this.ConnectionHandler.AttachToProcess(windowProcess);
+        }
+
+        private void FindCurrentPlayerChar()
+        {
+            // TODO: Add logic for getting char from window.
+            // Check if Bot is connected to game.
+            // Find TetBox with current player char and set it.
         }
     }
 }
